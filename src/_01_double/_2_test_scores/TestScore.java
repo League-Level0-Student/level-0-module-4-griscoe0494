fa16cf27495a05c9a0d8c48da9d726ce37464742
root <root@c18f1211940a>
2020-07-22 20:00:15 +0000

    Sleepy Head

--- src/_01_double/_2_test_scores/TestScore.java
+++ src/_01_double/_2_test_scores/TestScore.java
@@ -1,7 +1,23 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class TestScore {
 public static void main(String[]args) {
-	
+	String score = JOptionPane.showInputDialog(null, "What is your test score?");
+	double scoreAsInt = Integer.parseInt(score);
+	if(scoreAsInt >= 80) {
+		JOptionPane.showMessageDialog(null,"Wow! You must have studied very heard for that test!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You tried your best.");
+	}
 }
 }
+
+//Write code to ask the user for their test score.
+//Your program MUST allow the user to enter a test score that has a DECIMAL, e.g. 75.5
+//Give them a different message according to their score.
+//Example, for a good score: "Wow! You must have studied really hard for that test!"
+
+//HINT: Think about how you would convert a String to an int and use SIMILAR code to convert their test score to a double. 
--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -10,12 +10,12 @@ public class SleepyHead {
 		// boolean variables can only hold one of two values - true or false
 
 		/************************  SLEEPY HEAD  **************************/
-		boolean isWeekend;
+		boolean isWeekend= false;
 		// Write code to ask the user what day it is.
-        
+        String day=JOptionPane.showInputDialog(null,"What day is it?");
 		// Set the boolean isWeekend based on the value they enter
 		
-		
+        
 		// If it is the weekend, tell the user they get to sleep in.
 		
 		// If it is not the weekend, tell them to get out of bed and go to school!
